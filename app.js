45e9eb361f61f5c6a6686c014705bdbdeb0a304e
Tianyi Ma <tn.ma@outlook.com>
2020-04-05 18:33:03 -0500

    Add download buttons

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var URL = window.URL || window.webkitURL;
 
 var body = document.querySelector('body');
 var sourceImg = document.querySelector('#source');
+var fileName = null;
 var canvases = [
   document.querySelector('#c28'),
   document.querySelector('#c56'),
   document.querySelector('#c112'),
 ];
+
+function download(buttonId, imageId, scale) {
+  if (fileName == null) {
+    return;
+  }
+  var button = document.getElementById(buttonId);
+  var image = document.getElementById(imageId).toDataURL('image/png').replace('image/png', 'image/octet-stream');
+  button.setAttribute('download', `${fileName.split('.')[0]}@${scale}px.png`);
+  button.setAttribute('href', image);
+}
 
 sourceImg.addEventListener('load', () => {
   canvases.forEach((e) => {
@@ -25,5 +36,6 @@ body.ondragover = (e) => {
 body.ondrop = (e) => {
   e.preventDefault();
   console.log(e.dataTransfer.files);
+  fileName = e.dataTransfer.files[0].name;
   sourceImg.src = URL.createObjectURL(e.dataTransfer.files[0]);
 };
