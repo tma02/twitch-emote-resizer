0a3b0903e900c5b27c318f02c71c3e0833962811
Tianyi Ma <tn.ma@outlook.com>
2021-03-31 20:04:01 -0500

    Add GitHub button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import DropZone from './components/DropZone';
 import info from './info.svg';
+import github from './github.svg';
 
 function App() {
+  const handleViewOnGithub = () => {
+    window.location.href = "https://github.com/tma02/twitch-emote-resizer";
+  };
   return (
     <div className="App">
       <h4>Twitch Emote Resizer</h4>
@@ -16,6 +20,10 @@ function App() {
           <br/>
           Image processing is done in your browser. Your image never leaves your device.
         </p>
+      </div>
+      <div className="GitHub" onClick={handleViewOnGithub}>
+        <img src={github} className="GitHubIcon"/>
+        View on GitHub
       </div>
       <div className="Spacer"/>
     </div>
