f384b88642ac576c33f7c45f2e8915ba756095da
Tianyi Ma <tn.ma@outlook.com>
2021-07-15 05:13:14 -0500

    Update design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import DropZone from './components/DropZone';
 import info from './info.svg';
+import icon from './twitch-emote-resizer.svg';
 import github from './github.svg';
 
 function App() {
@@ -9,14 +10,17 @@ function App() {
   };
   return (
     <div className="App">
-      <h4>Twitch Emote Resizer</h4>
+      <div className="TitleRow">
+        <img className="Icon" src={icon}/>
+        <div className="Title">Twitch Emote Resizer</div>
+      </div>
       <DropZone />
       <div className="Info">
         <img src={info} className="InfoIcon" alt="info icon" />
         <p>
-          This page will resize an image into the sizes Twitch requires for emotes and badges.
+          This site will resize an image into the sizes Twitch requires for emotes and badges.
           <br/>
-          If the resized image is larger than Twitch's size limit of 25KB, this page will also attempt to compress it.
+          If the resized image exceeds Twitch's size limit of 25KB, this site will attempt to compress it.
           <br/>
           Image processing is done in your browser. Your image never leaves your device.
         </p>
